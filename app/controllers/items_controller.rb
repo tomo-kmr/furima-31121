2b205efb293c9c285f2f7286edd18b409afacfc4
tomo-kmr <tkkt0082@gmail.com>
2020-10-30 12:55:29 +0900

    指摘事項修正2回目と購入機能実装後の要件実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,7 +48,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     item = Item.find(params[:id])
-    redirect_to action: :index unless current_user.id == item.user_id
+    redirect_to action: :index if current_user.id != item.user_id || item.order.present?
   end
 
   def set_item
--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe OrderDestination, type: :model do
       it '都道府県が選択されていない状態だと保存できないこと' do
         @order_destination.address_pref_id = 1
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include("Address pref must be other than 1")
+        expect(@order_destination.errors.full_messages).to include('Address pref must be other than 1')
       end
       it '市区町村が空だと保存できないこと' do
         @order_destination.address_city = ''
